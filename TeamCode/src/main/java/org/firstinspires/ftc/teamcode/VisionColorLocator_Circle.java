b6e284f8d843ada7f3225f2410afbe7dc40e3408
rhsftc <ronsearle@hotmail.com>
2025-09-22 15:43:47 -0500

    Added color locator opmode for circles.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionColorLocator_Circle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionColorLocator_Circle.java
@@ -68,7 +68,7 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Disabled
+//@Disabled
 @TeleOp(name = "Vision Color-Locator (Circle)", group = "test")
 public class VisionColorLocator_Circle extends LinearOpMode {
     @Override
@@ -160,7 +160,7 @@ public class VisionColorLocator_Circle extends LinearOpMode {
         VisionPortal portal = new VisionPortal.Builder()
                 .addProcessor(colorLocator)
                 .setCameraResolution(new Size(320, 240))
-                .setCamera(hardwareMap.get(WebcamName.class, "Webcam1"))
+                .setCamera(hardwareMap.get(WebcamName.class, "webcam1"))
                 .build();
 
         telemetry.setMsTransmissionInterval(100);   // Speed up telemetry updates for debugging.
