d8246c238109b77f50ac6f1c9f4b1571b4cd666c
rhsftc <ronsearle@hotmail.com>
2025-09-22 19:09:39 -0500

    Added imu opmode.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLocalization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLocalization.java
@@ -64,7 +64,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Concept: AprilTag Localization", group = "test")
+@TeleOp(name = "AprilTag Localization", group = "test")
 //@Disabled
 public class AprilTagLocalization extends LinearOpMode {
 
