f1bfd319d410f4089b7691eee3b25bac515be0e5
rhsftc <ronsearle@hotmail.com>
2025-09-26 11:07:51 -0500

    Add support for continuous rotation servo and update telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestServo.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.PwmControl;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
@@ -9,9 +10,16 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
 @TeleOp(name = "Test Servo", group = "test")
 //@Disabled
 public class TestServo extends OpMode {
+    public enum SERVO_TYPE {
+        STANDARD,
+        CONTINUOUS
+    }
+
     static final double INCREMENT = 0.1;
+    SERVO_TYPE servoType = SERVO_TYPE.STANDARD;
     // amount to slew servo each button press.
     private Servo servo;
+    private CRServo crServo;
     private double position = 0;
 
     @Override
@@ -26,53 +34,84 @@ public class TestServo extends OpMode {
     @Override
     public void init_loop() {
         if (gamepad1.aWasReleased()) {
+            if (servoType == SERVO_TYPE.STANDARD) {
+                servoType = SERVO_TYPE.CONTINUOUS;
+                position = 0.5;
+                crServo = hardwareMap.get(CRServo.class, "servo1");
+                servo = null;
+            } else {
+                servoType = SERVO_TYPE.STANDARD;
+                position = 0;
+                servo = hardwareMap.get(ServoImplEx.class, "servo1");
+                crServo = null;
+            }
 
+            telemetry.addData("Servo Type", servoType);
+            telemetry.update();
         }
     }
 
     @Override
     public void start() {
-        showTelemetry();
+        telemetry.clearAll();
+        showServoTelemetry();
         telemetry.update();
-        servo.setPosition(0);
+        if (servoType == SERVO_TYPE.STANDARD) {
+            servo.setPosition(0);
+        } else {
+            crServo.setPower(0);
+        }
     }
 
     @Override
     public void loop() {
-        if (gamepad1.leftBumperWasReleased()) {
-            position = 0;
-        }
+        switch (servoType) {
+            case STANDARD:
+                if (gamepad1.leftBumperWasReleased()) {
+                    position = 0;
+                }
 
-        if (gamepad1.rightBumperWasReleased()) {
-            position = 1;
-        }
+                if (gamepad1.rightBumperWasReleased()) {
+                    position = 1;
+                }
 
-        if (gamepad1.yWasReleased()) {
-            position = .5;
-        }
+                if (gamepad1.yWasReleased()) {
+                    position = .5;
+                }
 
-        if (gamepad1.dpadUpWasReleased()) {
-            position += INCREMENT;
-        }
+                if (gamepad1.dpadUpWasReleased()) {
+                    position += INCREMENT;
+                }
 
-        if (gamepad1.dpadDownWasReleased()) {
-            position -= INCREMENT;
-        }
+                if (gamepad1.dpadDownWasReleased()) {
+                    position -= INCREMENT;
+                }
 
-        if (gamepad1.backWasReleased()) {
-            ((PwmControl) servo).setPwmDisable();
-        }
+                if (gamepad1.backWasReleased()) {
+                    ((PwmControl) servo).setPwmDisable();
+                }
 
-        servo.setPosition(position);
-        showTelemetry();
+                servo.setPosition(position);
+                break;
+            case CONTINUOUS:
+                if (gamepad1.dpadLeftWasReleased()) {
+                    crServo.setPower(0);
+                }
+
+                if (gamepad1.dpadRightWasReleased()) {
+                    crServo.setPower(1);
+                }
+
+                showCRServoTelemetry();
+            default:
+        }
     }
 
     private void showConfigTelemetry() {
-        telemetry.addLine("a = Servo");
-        telemetry.addLine("b = CRServo");
+        telemetry.addLine("a = Toggle servo type");
     }
 
-    private void showTelemetry() {
+    private void showServoTelemetry() {
         telemetry.addLine("Disable Servo = back");
         telemetry.addLine("Left bumper = 0");
         telemetry.addLine("Right bumper = 1");
@@ -81,5 +120,11 @@ public class TestServo extends OpMode {
         telemetry.addLine("Dpad down: Decrease position");
         telemetry.addData("PWM Range", ((PwmControl) servo).getPwmRange());
         telemetry.addData("Position", position);
+    }
+
+    private void showCRServoTelemetry() {
+        telemetry.addLine("Dpad left: Stop");
+        telemetry.addLine("Dpad right: Full power");
+        telemetry.addData("Power", crServo.getPower());
     }
 }
