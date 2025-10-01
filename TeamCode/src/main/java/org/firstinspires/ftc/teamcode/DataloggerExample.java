07319565f4d839e7d7a34843c1a50eff03124b76
rhsftc <ronsearle@hotmail.com>
2025-10-01 16:04:55 -0500

    Add LynxModule support and update current telemetry display

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DataloggerExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DataloggerExample.java
@@ -73,6 +73,7 @@ public class DataloggerExample extends LinearOpMode {
     YawPitchRollAngles orientation;
     Datalogger datalogger;     // edit name to Datalogger, or the name you used
     double myHeading;
+    LynxModule lynxModule;
     double current;
     int readCount = 0;
     String datalogFilename = "myDatalog";   // modify name for each run
@@ -83,7 +84,8 @@ public class DataloggerExample extends LinearOpMode {
     public void runOpMode() {
         List<LynxModule> lynx = hardwareMap.getAll(LynxModule.class);
         for (LynxModule module : lynx) {
-            current = module.getCurrent(CurrentUnit.MILLIAMPS);
+            lynxModule = module;
+            current = lynxModule.getCurrent(CurrentUnit.MILLIAMPS);
         }
         // Get device from robot Configuration.
         imu = hardwareMap.get(IMU.class, "imu");
@@ -132,6 +134,13 @@ public class DataloggerExample extends LinearOpMode {
                 // Show live telemetry on the Driver Station screen.
                 telemetry.addData("Count", readCount);
                 telemetry.addData("myHeading", "%.1f", myHeading);
+                for (LynxModule module : lynx) {
+                    lynxModule = module;
+                    current = lynxModule.getCurrent(CurrentUnit.MILLIAMPS);
+                    telemetry.addData("Is parent", lynxModule.isParent());
+                }
+
+                telemetry.addData("Lynx Current", current);
                 telemetry.update();
                 dataTimer.reset();      // start the interval timer again
             }   // end if(timer)
