db45b3c3d6ab0464a09c6f7602baf46ec8456bd5
rhsftc <ronsearle@hotmail.com>
2025-12-20 15:31:06 -0600

    Enhance telemetry output in AprilTagLocalization to include range and bearing information

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLocalization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLocalization.java
@@ -230,6 +230,9 @@ public class AprilTagLocalization extends LinearOpMode {
                             detection.robotPose.getPosition().x,
                             detection.robotPose.getPosition().y,
                             detection.robotPose.getPosition().z));
+                    telemetry.addLine(String.format("RB %6.1f (inches) %6.1f (deg) ",
+                            detection.ftcPose.range,
+                            detection.ftcPose.bearing));
                     telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
                             detection.robotPose.getOrientation().getPitch(AngleUnit.DEGREES),
                             detection.robotPose.getOrientation().getRoll(AngleUnit.DEGREES),
@@ -243,6 +246,7 @@ public class AprilTagLocalization extends LinearOpMode {
 
         // Add "key" information to telemetry
         telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("RB  = Range (dist.), Bearing (angle)");
         telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
 
     }   // end method telemetryAprilTag()
